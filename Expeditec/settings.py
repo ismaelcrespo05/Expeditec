8886bae989bacf7ce275d26849dc4594119e2f6c
ismaelcrespo05 <156700447+ismaelcrespo05@users.noreply.github.com>
2025-05-25 00:16:52 -0400

    Add files via upload

--- Expeditec/settings.py
+++ Expeditec/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'RRHH',
     'Chat',
     'channels',
+    'Registro',
 ]
 
 MIDDLEWARE = [
--- Login/views.py
+++ Login/views.py
@@ -63,40 +63,42 @@ class Login(View):
         if not username or not password:
             return Login.Notificacion(request=request,Error="Todos los campos son obligatorios.")
         try:
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                auth_login(request, user)
-               
-                user_agent = get_user_agent(request)
-                
-                # Guardar información en la sesión
-                request.session['user_agent'] = str(user_agent)
-                request.session['ip_address'] = request.META.get('REMOTE_ADDR')
-                request.session['login_time'] = timezone.now().isoformat()
-                
-                # También puedes guardar los datos parseados directamente
-                request.session['device_info'] = {
-                    'navegador': user_agent.browser.family,
-                    'version': user_agent.browser.version_string,
-                    'sistema_operativo': user_agent.os.family,
-                    'dispositivo': 'Móvil' if user_agent.is_mobile else 
-                                'Tablet' if user_agent.is_tablet else 
-                                'Computadora' if user_agent.is_pc else 
-                                'Bot' if user_agent.is_bot else 'Desconocido'
-                }
-
-
-                request.session.set_expiry(settings.SESSION_COOKIE_AGE)
-                request.session.save()
-                
-                return redirigir_usuario(request=request)
-            return Login.Notificacion(request=request,Error="Credenciales incorrectas")   
-            
+            return Login.autenticar(request=request,username=username,password=password)
         except Exception as e:
             # Log del error para debugging
             return Login.Notificacion(request=request,Error=f'Error del servidor: {str(e)}')
+    
+    @staticmethod    
+    def autenticar(request,username,password):
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
             
+            user_agent = get_user_agent(request)
+            
+            # Guardar información en la sesión
+            request.session['user_agent'] = str(user_agent)
+            request.session['ip_address'] = request.META.get('REMOTE_ADDR')
+            request.session['login_time'] = timezone.now().isoformat()
+            
+            # También puedes guardar los datos parseados directamente
+            request.session['device_info'] = {
+                'navegador': user_agent.browser.family,
+                'version': user_agent.browser.version_string,
+                'sistema_operativo': user_agent.os.family,
+                'dispositivo': 'Móvil' if user_agent.is_mobile else 
+                            'Tablet' if user_agent.is_tablet else 
+                            'Computadora' if user_agent.is_pc else 
+                            'Bot' if user_agent.is_bot else 'Desconocido'
+            }
 
+
+            request.session.set_expiry(settings.SESSION_COOKIE_AGE)
+            request.session.save()
+            
+            return redirigir_usuario(request=request)
+        return Login.Notificacion(request=request,Error="Credenciales incorrectas")   
+            
 
 class Logout(View):
     def get(self,request:HttpRequest):
@@ -180,7 +182,5 @@ class Cambio_clave(View):
             
                     else:
                         return Cambio_clave.Notificacion(request=request,Error="El codigo no es correcto.",step=2,username=username)
-                    
-                    
         else:
             return redirigir_usuario(request)
